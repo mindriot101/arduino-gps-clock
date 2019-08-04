22c4d1e8611fd759f80ba8f3d9df1bd21c42e730
Simon Walker <s.r.walker101@googlemail.com>
2019-08-04 23:49:56 +0100

    Plan to implement hardware debouncing

--- gps.ino
+++ gps.ino
@@ -1,12 +1,11 @@
-#include <NMEAGPS.h>
 #include <GPSport.h>
-#include <Streamers.h>
 #include <HCMAX7219.h>
+#include <NMEAGPS.h>
 #include <SPI.h>
-
+#include <Streamers.h>
 
 /* AltSoftSerial uses:
- *  
+ *
  *  TX 9
  *  RX 8
  */
@@ -19,18 +18,18 @@ enum class Mode {
     Time,
     Satellites,
     Latitude,
-    Longitude
+    Longitude,
+    Altitude,
 };
 #define MODE_PIN 2
 
 static NMEAGPS gps;
-volatile Mode current_mode;
+volatile Mode current_mode = Mode::Time;
 static char print_buffer[9];
-
-// TODO(srw): debounce interrupt
 
 void updateMode() {
     Serial.println("Interrupt fired");
+
     switch (current_mode) {
         case Mode::Time:
             Serial.println("Time -> Satellites");
@@ -45,7 +44,11 @@ void updateMode() {
             current_mode = Mode::Longitude;
             break;
         case Mode::Longitude:
-            Serial.println("Longitude -> Time");
+            Serial.println("Longitude -> Altitude");
+            current_mode = Mode::Altitude;
+            break;
+        case Mode::Altitude:
+            Serial.println("Altitude -> Time");
             current_mode = Mode::Time;
             break;
     }
@@ -57,16 +60,15 @@ void setup() {
         ;
     Serial.println("GPS software start");
 
-    Serial.print  ( F("\nGPS quiet time is assumed to begin after a ") );
-    Serial.print  ( gps.string_for( LAST_SENTENCE_IN_INTERVAL ) );
-    Serial.println( F(" sentence is received.\n"
-                "  You should confirm this with NMEAorder.ino\n") );
+    Serial.print(F("\nGPS quiet time is assumed to begin after a "));
+    Serial.print(gps.string_for(LAST_SENTENCE_IN_INTERVAL));
+    Serial.println(F(" sentence is received.\n"
+                "  You should confirm this with NMEAorder.ino\n"));
     Serial.flush();
 
     gpsPort.begin(9600);
 
     // Select the mode we're in
-    current_mode = Mode::Longitude;
     attachInterrupt(digitalPinToInterrupt(MODE_PIN), updateMode, RISING);
 }
 
@@ -74,21 +76,16 @@ void loop() {
     while (gps.available(gpsPort)) {
         const gps_fix fix = gps.read();
 
-        Serial.println("Got a fix");
-
         if (current_mode == Mode::Time) {
             Serial.println("In time mode");
             NeoGPS::time_t dt = fix.dateTime;
 
-            snprintf(print_buffer, 9, "%02d.%02d.%02d",
-                    dt.hours,
-                    dt.minutes,
+            snprintf(print_buffer, 9, "%02d.%02d.%02d", dt.hours, dt.minutes,
                     dt.seconds);
 
-            Serial.println(print_buffer);
         } else if (current_mode == Mode::Satellites) {
             Serial.println("In satellite mode");
-            snprintf(print_buffer, 9, "%d", fix.satellites);
+            snprintf(print_buffer, 9, "sats %3d", fix.satellites);
             Serial.println(print_buffer);
 
         } else if (current_mode == Mode::Latitude) {
@@ -107,6 +104,14 @@ void loop() {
             int32_t rem = int32_t((lng - lng_deg) * 100);
 
             snprintf(print_buffer, 9, "lng %2d.%2d", lng_deg, rem);
+        } else if (current_mode == Mode::Altitude) {
+            Serial.println("In altitude mode");
+
+            float alt = fix.altitude();
+
+            char str_temp[6];
+            dtostrf(alt, 3, 2, str_temp);
+            snprintf(print_buffer, 9, "alt %s", str_temp);
         } else {
             Serial.println("Not in a known mode");
         }
