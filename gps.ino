7cc020d6611795cedd78e070ecee1d3136e553d2
Simon Walker <s.r.walker101@googlemail.com>
2019-08-04 23:23:07 +0100

    Add latitude and longitude

--- gps.ino
+++ gps.ino
@@ -18,28 +18,35 @@ HCMAX7219 leds(LOAD);
 enum class Mode {
     Time,
     Satellites,
-    Location,
+    Latitude,
+    Longitude
 };
 #define MODE_PIN 2
 
 static NMEAGPS gps;
-static gps_fix fix;
-volatile Mode MODE;
+volatile Mode current_mode;
+static char print_buffer[9];
+
+// TODO(srw): debounce interrupt
 
 void updateMode() {
     Serial.println("Interrupt fired");
-    switch (MODE) {
+    switch (current_mode) {
         case Mode::Time:
             Serial.println("Time -> Satellites");
-            MODE = Mode::Satellites;
+            current_mode = Mode::Satellites;
             break;
         case Mode::Satellites:
-            Serial.println("Satellites -> Location");
-            MODE = Mode::Location;
+            Serial.println("Satellites -> Latitude");
+            current_mode = Mode::Latitude;
             break;
-        case Mode::Location:
-            Serial.println("Location -> Time");
-            MODE = Mode::Time;
+        case Mode::Latitude:
+            Serial.println("Latitude -> Longitude");
+            current_mode = Mode::Longitude;
+            break;
+        case Mode::Longitude:
+            Serial.println("Longitude -> Time");
+            current_mode = Mode::Time;
             break;
     }
 }
@@ -59,47 +66,55 @@ void setup() {
     gpsPort.begin(9600);
 
     // Select the mode we're in
-    MODE = Mode::Time;
+    current_mode = Mode::Longitude;
     attachInterrupt(digitalPinToInterrupt(MODE_PIN), updateMode, RISING);
 }
 
 void loop() {
     while (gps.available(gpsPort)) {
-        fix = gps.read();
+        const gps_fix fix = gps.read();
 
-        switch(MODE) {
-            case Mode::Time:
-                NeoGPS::time_t dt = fix.dateTime;
+        Serial.println("Got a fix");
 
-                char buf[9];
-                snprintf(buf, 9, "%02d.%02d.%02d",
-                        dt.hours,
-                        dt.minutes,
-                        dt.seconds);
+        if (current_mode == Mode::Time) {
+            Serial.println("In time mode");
+            NeoGPS::time_t dt = fix.dateTime;
 
-#ifndef NDEBUG
-                Serial.print("Time: ");
-                Serial.println(buf);
-#endif
+            snprintf(print_buffer, 9, "%02d.%02d.%02d",
+                    dt.hours,
+                    dt.minutes,
+                    dt.seconds);
 
-                // Also update the display
-                leds.Clear();
-                leds.print7Seg(buf, 8);
-                leds.Refresh();
-                break;
-            case Mode::Satellites:
-                leds.Clear();
-                leds.print7Seg("sat", 8);
-                leds.Refresh();
-                Serial.println("Satellites mode not handled yet");
-                break;
-            case Mode::Location:
-                leds.Clear();
-                leds.print7Seg("loc", 8);
-                leds.Refresh();
-                Serial.println("Location mode not handled yet");
-                break;
+            Serial.println(print_buffer);
+        } else if (current_mode == Mode::Satellites) {
+            Serial.println("In satellite mode");
+            snprintf(print_buffer, 9, "%d", fix.satellites);
+            Serial.println(print_buffer);
+
+        } else if (current_mode == Mode::Latitude) {
+            Serial.println("In latitude mode");
+
+            float lat = fix.latitude();
+            int32_t lat_deg = int32_t(lat);
+            int32_t rem = int32_t((lat - lat_deg) * 100);
+
+            snprintf(print_buffer, 9, "lat %2d.%2d", lat_deg, rem);
+        } else if (current_mode == Mode::Longitude) {
+            Serial.println("In longitude mode");
+
+            float lng = fix.longitude();
+            int32_t lng_deg = int32_t(lng);
+            int32_t rem = int32_t((lng - lng_deg) * 100);
+
+            snprintf(print_buffer, 9, "lng %2d.%2d", lng_deg, rem);
+        } else {
+            Serial.println("Not in a known mode");
         }
 
+        leds.Clear();
+        leds.print7Seg(print_buffer, 8);
+        leds.Refresh();
+
+        Serial.println(print_buffer);
     }
 }
